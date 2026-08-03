5ccbf91b3247d132ce06bd5bda2e926c0303d8c4
bimarsha <bimarshaghimire09@gmail.com>
2026-08-03 23:59:16 +0545

    binarySearch

---
+++ DSA/src/com/example/NEETCODE/BinarySearch.java
@@ -0,0 +1,40 @@
+package com.example.NEETCODE;
+
+public class BinarySearch {
+
+    public static void main(String[] args) {
+        int[] arr = {-1, 0, 2, 4, 6, 8};
+        int target = 4;
+
+        int result = binarySearch(arr, target);
+
+        System.out.println("Element found at index: " + result);
+    }
+
+    public static int binarySearch(int[] arr, int target) {
+
+       int left = 0;
+       int right = arr.length - 1;
+
+       int finalAns = searchTarget(arr , left , right , target);
+
+        return finalAns;
+    }
+
+    public static  int searchTarget(int[] arr , int left , int right , int target){
+        if(left > right){
+            return -1;
+        }
+        int mid = (left + right)/2;
+        
+        if(target < arr[mid]){
+          return  searchTarget(arr, left, mid-1, target);
+        }
+        else if(target > arr[mid]){
+           return searchTarget(arr, mid+1, right, target);
+        }
+        else {
+            return mid;
+        }
+    }
+}
